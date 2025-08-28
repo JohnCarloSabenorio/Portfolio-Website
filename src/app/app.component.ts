492a1fa74edffdaf8b49a6bd481642342cb1161b
Bluem <140798194+JohnCarloSabenorio@users.noreply.github.com>
2025-08-28 15:49:47 +0800

    added education and certification section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { IntroComponent } from './intro/intro.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { CertificatesComponent } from './certificates/certificates.component';
 import { Sign } from 'crypto';
 @Component({
   selector: 'app-root',
@@ -13,6 +15,8 @@ import { Sign } from 'crypto';
     IntroComponent,
     ProjectsComponent,
     SkillsComponent,
+    EducationComponent,
+    CertificatesComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
